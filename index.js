f9090597f40334121d3f9ccaa577cd19b5061323
0Blanck0 <50865149+0Blanck0@users.noreply.github.com>
2021-03-22 18:03:12 +0100

    Add help command

--- index.js
+++ index.js
@@ -32,7 +32,17 @@ Client.once('ready', () => {
 
 Client.on('guildCreate', (new_guild) => {
     //add_new_server(new_guild.name, new_guild, new_guild.owner.user.tag, new_guild.memberCount);
-    console.log(Client.users.resolve(new_guild.ownerID));
+    // new_guild.members.fetch()
+    // .then(console.log)
+    // .catch(console.error);
+    new_guild.members.fetch(new_guild.ownerID)
+    .then((result) => {
+        console.log(result.user);
+
+        if (result.user.id == new_guild.ownerID)
+            console.warn("ok");
+    })
+    .catch(console.error);
     sebas_std_logs("new on a server");
 });
 
@@ -40,24 +50,30 @@ Client.on('guildCreate', (new_guild) => {
 Client.on('message', (message) => {
     if (!message.author.bot) {
         switch (message.content) {
+            case Markdown + "help":
+                get_help(message);
+                break;
             case Markdown + "ping":
                 ping(message);
-                sebas_has_been("ping", message);
                 break;
             case Markdown + "hello":
                 hello(message);
-                sebas_has_been("hello", message);
                 break;
             case Markdown + "guild":
-                message.reply("Guild : " + message.guild);
-                sebas_std_logs("Guild");
+                get_guild_id(message);
                 break;
             default :
-                console.log(message.content);
+                if (message.content.indexOf(Markdown) == 0)
+                    message.reply("Sorry invalid command");
+
                 break;
         }
     }
 });
+
+/***************
+ * DB function *
+ ***************/
 
 function add_new_server(name, guild, owner, nb_numbers) 
 {
@@ -69,16 +85,83 @@ function add_new_server(name, guild, owner, nb_numbers)
     });
 }
 
+/*****************
+ * Tool function *
+ ****************/
+function get_banner(message)
+{
+    let banner = message.guild.bannerURL();
+
+    if (!banner)
+        banner = Client.user.avatarURL();
+    
+    return banner;
+}
+
+/******************
+ * Reply function *
+ *****************/
+
+// Function reply help message
+function get_help(message)
+{
+    const helpEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Help for Sebas Bot')
+	.setAuthor(message.author.tag, message.author.avatarURL())
+	.setDescription('here is a list of commands you can use :')
+	.setThumbnail(Client.user.avatarURL())
+	.addFields(
+        { name: '\u200B', value: '\u200B' },
+		{ name: 'help', value: 'Reply this embed with commands you can use for Sebas Bot' },
+		{ name: 'ping', value: 'Reply "Pong" with your ping in ms' },
+		{ name: 'hello', value: 'Reply just "Hello"' },
+        { name: '\u200B', value: '\u200B' }
+	)
+	.setImage(banner)
+	.setTimestamp()
+	.setFooter('Sebas Bot');
+
+    message.reply(helpEmbed);
+    sebas_has_been("help", message);
+}
+
 // Function reply to ping command
 function ping(message) 
 {
-    message.reply("Pong\nPing ms: " + (message.createdAt.getTime() - (new Date().getTime())));
+    let banner = get_banner(message);
+
+    const pingEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Ping command')
+	.setAuthor(message.author.tag, message.author.avatarURL())
+	.setDescription('Bot reply :')
+	.setThumbnail(Client.user.avatarURL())
+	.addFields(
+        { name: '\u200B', value: '\u200B' },
+		{ name: 'Pong !', value: "Ping ms: " + (message.createdAt.getTime() - (new Date().getTime())) },
+        { name: '\u200B', value: '\u200B' }
+	)
+	.setImage(banner)
+	.setTimestamp()
+	.setFooter('Sebas Bot');
+    
+    message.reply(pingEmbed);
+    sebas_has_been("ping", message);
 }
 
 // Function reply to hello command
 function hello(message)
 {
     message.reply("Hello !");
+    sebas_has_been("hello", message);
+}
+
+// Function reply guild id
+function get_guild_id(message)
+{
+    message.reply("Guild : " + message.guild);
+    sebas_std_logs("Guild");
 }
 
 // Function send log in console
@@ -87,6 +170,7 @@ function sebas_has_been(command, message)
     console.log("Sebas has been " + command + " by " + message.author.tag);
 }
 
+// Fonction send generique log in console
 function sebas_std_logs(command)
 {
     console.log(new Date() + ": commande " + command + " has been run");
